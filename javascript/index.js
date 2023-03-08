5e2821e2678ff9cd63a914d6872e2487e0544ede
Odile-a <odilealvarez@gmail.com>
2023-03-08 12:11:38 +0000

    Set SanDiego clock and upgrade JS JS
    SetInterval

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,19 @@
+function updateTime(){
 // London
 let londonElement = document.querySelector("#london");
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time")
 let londonTime = moment().tz("Europe/London");
 londonDateElement.innerHTML = londonTime.format("ddd Do MMM YYYY");
-londonTimeElement.innerHTML = londonTime.format("h:mm:ss  [<small>AM</small>]");
+londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+
+//San Diego
+let sanDiegoElement = document.querySelector("#sanDiego");
+let sanDiegoDateElement = sanDiegoElement.querySelector(".date");
+let sanDiegoTimeElement = sanDiegoElement.querySelector(".time")
+let sanDiegoTime = moment().tz("America/Vancouver");
+sanDiegoDateElement.innerHTML = sanDiegoTime.format("ddd Do MMM YYYY");
+sanDiegoTimeElement.innerHTML = sanDiegoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
